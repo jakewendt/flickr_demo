89777ddd530827f5d4ad8b77c89b46efb513bea2
Jake <github@jakewendt.com>
2011-10-09 11:35:06 -0700

    Included rails and rack to make production host happy.

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,8 @@ RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-	config.gem "flickr"	#	included
+	config.gem "flickr"
+	config.gem "rack", :version => "=1.1.2"
 	config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 	config.time_zone = 'UTC'
 end
